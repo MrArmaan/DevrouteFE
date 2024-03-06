153ebfed3072a65cf269ea9155fd4ab5fe0b3637
Jack Mears <jack.mears@ymail.com>
2024-03-06 17:49:06 +0000

    enhancement: Fix pages link bug

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,18 +1,54 @@
+import React, { useState } from "react";
 import "./Signup.css";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Signup form submitted");
+  };
+
   return (
     <div className="signup-container">
       <h1>Sign Up</h1>
-      <form className="signup-form">
-        <input placeholder="username" type="text" value={username} required />
+      <form className="signup-form" onSubmit={handleSubmit}>
+        <input
+          placeholder="username"
+          type="text"
+          value={username}
+          onChange={handleUsernameChange}
+          required
+        />
 
-        <input placeholder="email" type="email" value={email} required />
+        <input
+          placeholder="email"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
 
         <input
           placeholder="password"
           type="password"
           value={password}
+          onChange={handlePasswordChange}
           required
         />
 
