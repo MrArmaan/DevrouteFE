3c54c07aa00bdc46a349d014c25a179bdcc09879
Jack Mears <jack.mears@ymail.com>
2024-03-06 16:19:01 +0000

    enhancement: Create links between the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
 
-function App() {
+const App = () => {
   return (
-    <nav>
-      <div className="navbar-left">
-        <ul>
-          <li>Jobs</li>
-          <li>Log In</li>
-          <li>Sign Up</li>
-        </ul>
-      </div>
+    <Router>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <ul>
+            <li>
+              <Link to="/">Jobs</Link>
+            </li>
+            <li>
+              <Link to="/login">Log In</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-right">
+          <div className="profile-tab">{/* profile pic */}</div>
+        </div>
+      </nav>
 
-      <div className="navbar-right">
-        <div className="profile-tab">{/* profile pic */}</div>
-      </div>
-    </nav>
+      <Switch>
+        <Route path="/Jobsearch">
+          <Jobsearch />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
