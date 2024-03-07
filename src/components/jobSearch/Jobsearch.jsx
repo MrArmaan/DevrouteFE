b7082fc834a0b0ea627f894a7229cab65fdbe48f
xmikebx <xmikebx@gmail.com>
2024-03-07 10:58:32 +0000

    chore: create basic jobsearch page

---
+++ src/components/jobSearch/Jobsearch.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./jobsearch.css";
+
+const JobPage = () => {
+  return (
+    <div className="jobsearch-wrapper">
+      <div className="job-offer-wrapper">
+        <></>
+        <></>
+      </div>
+      <div className="search-options">
+        <button>Search By Name</button>
+        <button>Search By Location</button>
+      </div>
+      <img src="" alt="workplace type image" />
+    </div>
+  );
+};
+
+export default JobPage;
